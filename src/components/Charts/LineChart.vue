046687493a2c0d85c86a39231357509fdf52e47d
farrukh.ravilov <ravilovfarrux@gmail.com>
2025-01-11 12:21:19 +0500

    char js

--- src/components/Charts/LineChart.vue
+++ src/components/Charts/LineChart.vue
@@ -21,31 +21,31 @@ const root = ref(null)
 
 let chart
 
-// Chart.register(LineElement, PointElement, LineController, LinearScale, CategoryScale, Tooltip)
+Chart.register(LineElement, PointElement, LineController, LinearScale, CategoryScale, Tooltip)
 
-// onMounted(() => {
-//   chart = new Chart(root.value, {
-//     type: 'line',
-//     data: props.data,
-//     options: {
-//       responsive: true,
-//       maintainAspectRatio: false,
-//       scales: {
-//         y: {
-//           display: false
-//         },
-//         x: {
-//           display: true
-//         }
-//       },
-//       plugins: {
-//         legend: {
-//           display: false
-//         }
-//       }
-//     }
-//   })
-// })
+onMounted(() => {
+  chart = new Chart(root.value, {
+    type: 'line',
+    data: props.data,
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        y: {
+          display: false
+        },
+        x: {
+          display: true
+        }
+      },
+      plugins: {
+        legend: {
+          display: false
+        }
+      }
+    }
+  })
+})
 
 const chartData = computed(() => props.data)
 
